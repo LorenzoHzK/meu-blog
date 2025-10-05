4bb12485632360ee13d18f201256d44dca764ad4
LorenzoHzK <matheuslorenzosiquiera@gmail.com>
2025-10-05 17:38:31 -0300

    add icons and select the directory of icons and imagems

--- slate.config.ts
+++ slate.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from './src/helpers/config-helper';
 export default defineConfig({
   lang: 'en-US',
   site: 'https://lorenzohzk.github.io/meu-blog',
-  avatar: './avatar.jpeg',
+  avatar: '/meu-blog/avatar.jpeg',
   title: 'Meu blog',
   description: 'Neste blog pretendo colocar algumas informações, e também colocar algumas coisas que eu estou estudando ao longo da minha jornada',
   lastModified: true,
